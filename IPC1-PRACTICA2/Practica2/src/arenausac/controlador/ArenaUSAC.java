44fc31cf90678468274a9e4316ce6cf1ab4a8ed4
YasminMiranda03 <3946136940101@ingenieria.usac.edu.gt>
2025-09-24 23:42:29 -0600

    controlador

--- IPC1-PRACTICA2/Practica2/src/arenausac/controlador/ArenaUSAC.java
+++ IPC1-PRACTICA2/Practica2/src/arenausac/controlador/ArenaUSAC.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package arenausac.controlador;
+import arenausac.modelo.Personaje;
 
 /**
  *
  * @author APROJUSA
  */
 public class ArenaUSAC {
+    private Personaje[] personajes;
+    private int cantidad;
     
+    public ArenaUSAC(int maxPersonajes){
+        personajes = new Personaje[maxPersonajes];
+        cantidad = 0;
+    }
+    
+    public void agregarPersonaje(Personaje p){
+        if (cantidad >= personajes.length){
+            System.out.println("Error, no se pueden agregar mas personajes");
+            return;
+        }
+        
+        for (int i = 0; i< cantidad; i++){
+            if (personajes[i].getNombre().equalsIgnoreCase(p.getNombre())){
+                System.out.println("Error, ya existe un personaje con ese nombre");
+                return;
+            }
+        }
+        
+        personajes[cantidad] = p;
+        cantidad++;
+        System.out.println("Personaje agregado: " + p.getNombre());
+    }
 }
--- IPC1-PRACTICA2/Practica2/src/arenausac/modelo/Personaje.java
+++ IPC1-PRACTICA2/Practica2/src/arenausac/modelo/Personaje.java
@@ -24,6 +24,62 @@ public class Personaje {
         this.nombre = nombre;
         this.arma = arma;
         
+        if (hp < 100) {
+            this.hp = 100;
+        } else if (hp > 500) {
+            this.hp = 500;
+        } else {
+            this.hp = hp;
+        }
+
+        if (ataque < 10) {
+            this.ataque = 10;
+        } else if (ataque > 100) {
+            this.ataque = 100;
+        } else {
+            this.ataque = ataque;
+        }
+
+        if (velocidad < 1) {
+            this.velocidad = 1;
+        } else if (velocidad > 10) {
+            this.velocidad = 10;
+        } else {
+            this.velocidad = velocidad;
+        }
+
+        if (agilidad < 1) {
+            this.agilidad = 1;
+        } else if (agilidad > 10) {
+            this.agilidad = 10;
+        } else {
+            this.agilidad = agilidad;
+        }
+
+        if (defensa < 1) {
+            this.defensa = 1;
+        } else if (defensa > 50) {
+            this.defensa = 50;
+        } else {
+            this.defensa = defensa;
+        }
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
+    public String getArma() { return arma; }
+    public int getHp() { return hp; }
+    public int getAtaque() { return ataque; }
+    public int getVelocidad() { return velocidad; }
+    public int getAgilidad() { return agilidad; }
+    public int getDefensa() { return defensa; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | " + nombre + " (" + arma + ") "
+             + "HP:" + hp + " Atk:" + ataque
+             + " Vel:" + velocidad + " Agi:" + agilidad + " Def:" + defensa;
     }
     
 }
