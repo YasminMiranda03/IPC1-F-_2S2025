7c732021f6d0cbe1ec1e508dc12ee334762fb57a
YasminMiranda03 <3946136940101@ingenieria.usac.edu.gt>
2025-09-24 23:40:51 -0600

    controlador

--- IPC1-PRACTICA2/Practica2/src/arenausac/modelo/Personaje.java
+++ IPC1-PRACTICA2/Practica2/src/arenausac/modelo/Personaje.java
@@ -6,8 +6,24 @@ package arenausac.modelo;
 
 /**
  *
- * @author APROJUSA
+ * @author Katherin Yasmin
  */
 public class Personaje {
+    private static int contadorId = 1;
+    private int id;
+    private String nombre;
+    private String arma;
+    private int hp;
+    private int ataque;
+    private int velocidad;
+    private int agilidad;
+    private int defensa;
+    
+    public Personaje(String nombre, String arma, int hp, int ataque, int velocidad, int agilidad, int defensa){
+        this.id = contadorId++;
+        this.nombre = nombre;
+        this.arma = arma;
+        
+    }
     
 }
