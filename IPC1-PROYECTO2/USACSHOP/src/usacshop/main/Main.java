eb49c5b3c5df120b5157318ae90461b28cce582a
YasminMiranda03 <3946136940101@ingenieria.usac.edu.gt>
2025-10-07 12:47:42 -0600

    creacion del login

--- IPC1-PROYECTO2/USACSHOP/src/usacshop/main/Main.java
+++ IPC1-PROYECTO2/USACSHOP/src/usacshop/main/Main.java
@@ -2,19 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package usacshop;
+package usacshop.main;
 
 /**
  *
- * @author APROJUSA
+ * @author Katherin Yasmin
  */
-public class USACSHOP {
 
+import usacshop.vista.LoginView;
+
+public class Main {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        java.awt.EventQueue.invokeLater(() -> {
+            new LoginView().setVisible(true);
+        });
     }
     
 }
