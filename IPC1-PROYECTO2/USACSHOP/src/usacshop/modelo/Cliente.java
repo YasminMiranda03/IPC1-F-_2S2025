f6fc47303667ac41cd3a53a2de9a3a3ab0ef6b78
YasminMiranda03 <3946136940101@ingenieria.usac.edu.gt>
2025-10-21 16:49:04 -0600

    cliente/modelo

--- IPC1-PROYECTO2/USACSHOP/src/usacshop/modelo/Cliente.java
+++ IPC1-PROYECTO2/USACSHOP/src/usacshop/modelo/Cliente.java
@@ -4,19 +4,49 @@
  */
 package usacshop.modelo;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Katherin Yasmin
  */
-public class Cliente extends Usuario{
+public class Cliente implements Serializable{
+    private String codigo;
+    private String nombre;
+    private String genero;
     private String fechaNacimiento;
     
-    public Cliente(String codigo, String nombre, String genero, String contraseña){
-        super(codigo, nombre, genero, contraseña);
+    public Cliente(String codigo, String nombre, String genero, String fechaNacimiento){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.genero = genero;
         this.fechaNacimiento = fechaNacimiento;
     }
     
+    
+    public String getCodigo(){
+        return codigo;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getGenero(){
+        return genero;
+    }
     public String getFechaNacimiento(){
         return fechaNacimiento;
+    }
+    
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+    public void setFechaNacimiento(String fechaNacimiento){
+        this.fechaNacimiento = fechaNacimiento;
     }
 }
